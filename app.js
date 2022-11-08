4b4416dab1d708a6ce390f05b07534aef2f8ac5e
shubham <shubhamsarkar2587@gmail.com>
2022-11-08 17:58:16 +0530

    Title: project completed Dev: Shubham Date: 8 Nov 2022

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./db/connect');
+const notFound = require('./middlewares/notFound.middleware');
 require('dotenv').config();
 
 const app = express();
@@ -12,11 +13,14 @@ app.use(express.json());
 
 // routes
 app.use('/api/v1/tasks', tasks);
+app.use(notFound);
+
+const port = process.env.PORT || 3000
 
 const start = async () => {
     try {
         await connectDB(process.env.MONGO_DB_URI)
-        app.listen(3000, () => console.log(`listening to 3000 port ...`))
+        app.listen(port, () => console.log(`listening to 3000 port ...`))
     } catch (err) {
         console.log(err);
     };
